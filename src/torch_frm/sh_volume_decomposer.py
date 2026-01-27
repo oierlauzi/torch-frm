56cff012d4ccb870c8781a4fa7cd638eabeb9cf2
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2026-01-27 18:06:53 +0100

    Added volume decomposer

--- src/torch_frm/sh_volume_decomposer.py
+++ src/torch_frm/sh_volume_decomposer.py
@@ -2,7 +2,7 @@ from typing import Optional
 import torch
 import math
 
-from torch_frm import sample_3d
+from torch_frm import sample_3d, spherical_harmonics, wigner_matrices
 from torch_frm import spherical_harmonics
 
 def _spherical_to_cartesian(
@@ -72,9 +72,46 @@ class SHVolumeDecomposer:
         
         self._weights = \
             torch.sin(self.theta_grid_)*self.spherical_harmonics_.conj()
+            
+        self._wigner_half_pi = wigner_matrices(
+            torch.tensor(0.5*math.pi, dtype=dtype, device=device),
+            self.bandwidth_
+        )
 
     def transform(self, volume: torch.Tensor) -> torch.Tensor:
         shells = sample_3d(volume, self.cartesian_)
         shells = shells.to(self.spherical_harmonics_.dtype)
-        return torch.einsum('kij,hij->kh', shells, self._weights)
+        return torch.einsum('kij,hij,k->kh', shells, self._weights, self.radii_)
     
+    def compute_rcf(self, x_sh: torch.Tensor, y_sh: torch.Tensor) -> torch.Tensor:
+        dtype = torch.promote_types(x_sh.dtype, y_sh.dtype)
+        rct_ft = torch.zeros(
+            (2*self.bandwidth_, )*3, 
+            dtype=dtype, 
+            device=x_sh.device
+        )
+
+        start_1d = 0
+        start_2d = 0
+        for l in range(self.bandwidth_):
+            count = 2*l + 1
+            end_1d = start_1d + count
+            end_2d = start_2d + count*count
+            
+            d = self._wigner_half_pi[start_2d:end_2d].view(count, count)
+            i = torch.einsum(
+                'ki,kj->ij', 
+                x_sh[start_1d:end_1d], 
+                y_sh[start_1d:end_1d].conj()
+            )
+            
+            term = d[:,:,None]*d[None,:,:]*i[:,None,:]
+            central_range = slice(self.bandwidth_ - l, self.bandwidth_ + l + 1)
+            rct_ft[central_range,central_range,central_range] += term
+            
+            start_1d = end_1d
+            start_2d = end_2d
+            
+        rct_ft = torch.fft.fftshift(rct_ft)
+        return torch.fft.ifftn(rct_ft)
+            
